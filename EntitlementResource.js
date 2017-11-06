2912436540ccc13363d7162dbc92b48e17fb0b29
Andrey Davydov <webintoit@gmail.com>
2017-11-06 15:14:02 +0200

    custom entitlement api

--- EntitlementResource.js
+++ EntitlementResource.js
@@ -36,6 +36,65 @@ class EntitlementResource extends HttpResource {
         });
     }
 
+    _getEvaluatingBaseUri(){
+        return `/clients/${this._client.clientInfo.id}/authz/resource-server/policy/evaluate`;
+    }
+
+    /*** Hack:  custom protected request via admin evaluation api **/
+    getByResourceType(userId, resourceType, context = {}, clientId = this._client.clientId){
+
+        let uri = `${this._client.url}/auth/admin/realms/${this._client.realm}/${this._getEvaluatingBaseUri()}`;
+
+        this._client.refreshGrant().then(()=>{
+            let options = {
+                method: 'POST',
+                uri: uri,
+                headers: {
+                    "Authorization": `Bearer ${this._client.grant.access_token.token}`
+                },
+                json: true,
+                body: {
+                    clientId: clientId,
+                    context: {
+                        attributes: context
+                    },
+                    userId: userId,
+                    resources: [
+                        {
+                           type: resourceType,
+                           scope: []
+                        }
+                    ]
+                }
+            };
+            return request(options);
+        }).catch(response =>{
+            throw new Error(response.error.errorMessage);
+        });
+
+
+    }
+
+    getByPermissions(access_token, permissions, clientId = this._client.clientId){
+        this._client.refreshGrant().then(()=>{
+            let options = {
+                method: 'POST',
+                uri: this._prepareUri('/authz/entitlement/' + clientId),
+                headers: {
+                    "Authorization": `Bearer ${access_token}`
+                },
+                json: true,
+                body: {
+                    permissions: permissions
+                }
+            };
+            return request(options);
+        }).catch(response =>{
+            throw new Error(response.error.errorMessage);
+        });
+    }
+
+
     validateToken(token){
         if(typeof token === "string"){
             token = new Token(token, this._client.clientId);
